ef29c4154cfabc61ff628182504d920f22b766b7
Willy Damásio <108426075+damaazsio@users.noreply.github.com>
2022-09-27 13:46:53 -0300

    Add files via upload

---
+++ Exercicios.py
@@ -0,0 +1,25 @@
+def saudaçao(nome, bomdia):
+    print(f'{nome}, {bomdia}')
+
+
+saudaçao('willy','boa tarde')
+saudaçao('maria', 'boa tarde')
+saudaçao('carlos', 'hola mundo')
+
+
+
+def soma (n1,n2):
+    
+    print(n1+n2)
+soma(6,5)
+soma(10,10000000)
+soma(9,0)
+
+
+def valor1(valor, percentual):
+    print(valor + valor * percentual / 100)
+
+valor1 (10,80)
+
+
+
